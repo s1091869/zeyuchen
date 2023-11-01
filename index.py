4790e91e05ba07491c8a84b5681268e6c28e0566
s1091869 <s1091869@gm.pu.edu.tw>
2023-11-01 21:34:03 +0800

    version 4

--- index.py
+++ index.py
@@ -7,63 +7,41 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-	homepage = "<h1>陳則諭Python網頁</h1>"
+	homepage = "<h1>資工四B 410918699 陳則諭的求職資訊</h1>"
 
-	homepage += "<a href=/mis>MIS</a><br>"
+	homepage += "<a href=/about>我的個人簡介</a><br>"
+	
+	homepage += "<a href=/today>職涯測驗結果</a><br>"
 
-	homepage += "<a href=/today>顯示日期時間</a><br>"
+	homepage += "<a href=/welcome>求職履歷自傳</a><br>"
 
-	homepage += "<a href=/welcome?nick=zeyuchen>傳送使用者暱稱</a><br>"
-
-	homepage += "<a href=/about>陳則諭簡介網頁</a><br>"
-	homepage += "<a href=/account>帳密傳值</a><br>"
+	homepage += "<a href=/mis>MIS相關工作介紹</a><br>"
 
 	return homepage
-@app.route("/welcome", methods=["GET", "POST"])
-
-def welcome():
-
-	user = request.values.get("nick")
-
-	return render_template("welcome.html", name=user)
 
 @app.route("/about")
 
 def about():
 	return render_template("aboutme.html")
 
-@app.route("/mis")
-
-def course():
-
-	return "<h1>資訊管理導論</h1>"
-
 @app.route("/today")
 
 def today():
 
-	now = datetime.now()
+	return render_template("today.html")
 
-	return render_template("today.html", datetime = str(now))
+@app.route("/welcome")
+
+def welcome():
+	
+	return render_template("welcome.html")
+
+@app.route("/mis")
+
+def course():
+	return render_template("mis.html")
 
 
-@app.route("/account", methods=["GET", "POST"])
-
-def account():
-
-	if request.method == "POST":
-
-		user = request.form["user"]
-
-		pwd = request.form["pwd"]
-
-		result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd
-
-		return result
-
-	else:
-
-		return render_template("account.html")
 
 
 #if __name__ == "__main__":
